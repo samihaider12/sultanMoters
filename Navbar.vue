bd4190875bd8b070acff7adf4bf966cf99819bf4
samihaider12 <samihaider687@gmail.com>
2025-06-06 04:42:46 -0700

    Add files via upload

--- Navbar.vue
+++ Navbar.vue
@@ -2,36 +2,44 @@
   <nav class="navbar navbar-expand-lg mt-0" id="navbar">
     <div class="container-fluid d-flex align-items-center justify-content-between">
 
+      <!-- Sidbar part -->
       <div class="d-flex align-items-center">
-        <div class="menu-toggle me-5" @click="$emit('toggle-sidebar')">
-           
+        <div class="menu-toggle me-5" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+          aria-expanded="false" >
           <span class="bar" :class="{ topopen: isOpen }"></span>
           <span class="bar" :class="{ midopen: isOpen }"></span>
           <span class="bar" :class="{ botopen: isOpen }"></span>
         </div>
+        <div class="dropdown">
+          <ul class="list-unstyled mt-5 dropdown-menu" aria-labelledby="dropdownMenuButton1" id="headDropdown">
+            <div class="container-fluid">
+
+              <h5 class="d-lg-none me-3">
+                <li><router-link to="/" class="dropdown-item">Home</router-link></li>
+                <li><router-link to="/about" class="dropdown-item">About</router-link></li>
+                <li><router-link to="/Service" class="dropdown-item">Services</router-link></li>
+                <li><router-link to="/contact" class="dropdown-item">Contact</router-link></li>
+              </h5>
+
+              <h5>
+                <li class="sBar"><router-link to="/bike" class="nav-link">Bike</router-link></li>
+                <li class="sBar"><router-link to="/car" class="nav-link">Car</router-link></li>
+                <li class="sBar"><router-link to="/bus" class="nav-link">Bus</router-link></li>
+                <li class="sBar"><router-link to="/car4x4" class="nav-link">4x4</router-link></li>
+                <li class="sBar"><router-link to="" class="nav-link">Trola</router-link></li>
+                <li class="sBar"><router-link to="" class="nav-link">Police Van</router-link></li>
+                <li class="sBar"><router-link to="" class="nav-link">Army Tank</router-link></li>
+              </h5>
+            </div>
+          </ul>
+        </div>
+
+
         <img src="../images/icon.png" alt="Logo" class="me-5 logo-img" style="height: 60px;" />
       </div>
- 
-      <!-- ✅ Dropdown on md and smaller -->
-      <div class="dropdown d-lg-none me-3">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-           id="dropdownMenuLink"
-          data-bs-toggle="dropdown" 
-          aria-expanded="false"
-        >
-       <b> Home</b>
-        </button>
-        <ul  class="dropdown-menu " aria-labelledby="dropdownMenuLink" id="menu" style="background: silver;">
-          <b><li><router-link to="/" class="dropdown-item">Home</router-link></li>
-          <li><router-link to="/about" class="dropdown-item">About</router-link></li>
-          <li><router-link to="/Service" class="dropdown-item">Services</router-link></li>
-          <li><router-link to="/contact" class="dropdown-item">Contact</router-link></li>
-        <li><router-link to="/Login" class="dropdown-item text-success">Login</router-link></li>
-        <li><router-link to="/SignUp" class="dropdown-item text-primary">Sign Up</router-link></li></b>
-      </ul>
-      </div>
+
+
+<!-- Navbar part -->
 
       <!-- ✅ Inline menu on lg and above -->
       <div class="collapse navbar-collapse justify-content-between  " id="navbarNav">
@@ -55,7 +63,7 @@
       </div>
 
       <!-- Login/SignUp Always Visible -->
-      <div class="d-flex d-sm-block d-none ms-2" style="position: fixed; right: 10px ;" id="login">
+      <div class="d-flex  ms-2" style="position: fixed; right: 10px ;" id="login">
         <router-link to="/Login" class="btn btn-outline-success me-2">Login</router-link>
         <router-link to="/SignUp" class="btn btn-outline-primary">Sign Up</router-link>
       </div>
@@ -63,39 +71,48 @@
   </nav>
 </template>
 <script setup>
+
+// import { useSidebar } from '@/composables/useSidebar.js'
+// import {useSidebar} from '@/components/useSidebar.js'
+// const { toggleSidebar, isSidebarOpen } = useSidebar();
+
 const props = defineProps({
   isOpen: Boolean
 })
+
 </script>
 
 <style scoped>
-li .nav-link{
- 
+li .nav-link {
+
   padding: 9px;
   margin: 0 15px;
   border-radius: 5px;
-   
+
 }
-li .nav-link:hover{
+
+li .nav-link:hover {
   /* background: #A8F1FF; */
-  
-  background:   rgb(239, 239, 239);
+
+  background: rgb(239, 239, 239);
 }
-#pic{
+
+#pic {
   position: relative;
   left: -157px;
 }
- 
+
 #navbar {
   /* background: #4ED7F1; */
   background: silver;
   position: fixed;
   top: 0;
-  left: 0;  
+  left: 0;
   width: 100%;
-  z-index: 1000; /* Ensure it's above other content */
+  z-index: 1000;
+  /* Ensure it's above other content */
   height: 70px;
-   /* Adjust as per your design */
+  /* Adjust as per your design */
 }
 
 /* sideBar Component */
@@ -119,7 +136,7 @@ li .nav-link:hover{
 }
 
 /* Cross Animation */
-.topopen {
+/* .topopen {
   transform: translateY(7px) rotateZ(45deg);
 }
 .midopen {
@@ -127,60 +144,47 @@ li .nav-link:hover{
 }
 .botopen {
   transform: translateY(-7px) rotateZ(-45deg);
-}
+} */
 
 
-  /* home dropdown */
+/* home dropdown */
 /* Inside <style scoped> */
 .menu-toggle {
   display: flex;
   flex-direction: column;
   cursor: pointer;
 }
+
 .menu-toggle .bar {
   height: 4px;
   width: 25px;
-  background-color: black; /* or black based on navbar */
+  background-color: black;
+  /* or black based on navbar */
   /* margin: 2% 0; */
   transition: 0.3s;
 }
-#navbarNav{
+
+#navbarNav {
   font-size: 22px;
 }
-#navDropdown{
+
+#navDropdown {
   margin-left: 600px;
 }
+
 .dropdown-menu .dropdown-item:hover {
   background-color: silver;
 }
 
-#dropdownMenuLink{
-  position:  absolute;
-  top: -19px;
-  right: 150px;
-}
-
-#menu{
-  position:  absolute;
-  top: 30px;
-  left: -235px;
-}
-
-@media screen and (max-width: 576px) {
-  body {
-  #dropdownMenuLink{
-  position:  absolute;
-  top: -20px;
-  right: 20px;
-}
-
-#menu{
-  position:  absolute;
-  top: 30px;
-  left: -180px;
-}
-
-  }
+#headDropdown {
+  position: fixed;
+  top: 20.5px;
+  width: 200px;
+  height: 100vh;
+  border-radius: 0px;
+  color: black;
+  background-color: rgb(204, 203, 203);
+   transition: margin-left 0.4s ease-in-out;
+    
 }
 </style>
-
